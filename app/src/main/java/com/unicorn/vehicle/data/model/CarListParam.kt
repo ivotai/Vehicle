f53dd981bf004f1b9447dbc10ee3dc009b56b04c
ivotai <ivotai@163.com>
2019-10-10 14:51:25 +0800

    修改意见。

--- app/src/main/java/com/unicorn/vehicle/data/model/CarListParam.kt
+++ app/src/main/java/com/unicorn/vehicle/data/model/CarListParam.kt
@@ -2,7 +2,7 @@ package com.unicorn.vehicle.data.model
 
 data class CarListParam(
     val carType: Int? = null,
-    val carState: Int? = null,
+    val carState: Int? = 0,
     val name: String? = null,
     val no: String? = null
 )
