0e8710eeb6958c9b70c93e1d924648f6c587b29c
ivotai <ivotai@163.com>
2020-04-10 13:30:46 +0800

    refactoring

---
+++ app/src/main/java/com/unicorn/vehicle/data/model/CarUsageLog.kt
@@ -0,0 +1,3 @@
+package com.unicorn.vehicle.data.model
+
+class CarUsageLog
---
+++ app/src/main/java/com/unicorn/vehicle/data/model/param/CarUsageLogListParam.kt
@@ -0,0 +1,10 @@
+package com.unicorn.vehicle.data.model.param
+
+data class CarUsageLogListParam(
+    val carID: Int? = null,
+//    val userID: String? = null,
+    val carNo: String? = null,
+    val eventType: Int? = null,
+    val startTime: String? = null,
+    val endTime: String? = null
+)
