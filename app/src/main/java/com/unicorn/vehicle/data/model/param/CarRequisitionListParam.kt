a60001b8a8a3701c93c2e98ca8fe41db9cc39e01
ivotai <ivolianer2015@163.com>
2020-07-16 11:20:59 +0800

    CarRequisitionListParam.kt 添加 orgId

--- app/src/main/java/com/unicorn/vehicle/data/model/param/CarRequisitionListParam.kt
+++ app/src/main/java/com/unicorn/vehicle/data/model/param/CarRequisitionListParam.kt
@@ -1,7 +1,10 @@
 package com.unicorn.vehicle.data.model.param
 
+import com.unicorn.vehicle.app.orgId
+
 data class CarRequisitionListParam(
     val requisitionCarType: Int? = null,
     val approvalUserID: String? = null,
-    val states: List<Int>
+    val states: List<Int>,
+    val orgID: Int = orgId
 )
