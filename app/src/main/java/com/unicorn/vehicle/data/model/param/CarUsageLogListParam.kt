4960bb8786d11bd7a3202a53c706b2eedea041de
ivotai <ivolianer2015@163.com>
2020-07-16 11:22:59 +0800

    CarUsageLogListParam.kt 添加 orgId

--- app/src/main/java/com/unicorn/vehicle/data/model/param/CarUsageLogListParam.kt
+++ app/src/main/java/com/unicorn/vehicle/data/model/param/CarUsageLogListParam.kt
@@ -1,10 +1,13 @@
 package com.unicorn.vehicle.data.model.param
 
+import com.unicorn.vehicle.app.orgId
+
 data class CarUsageLogListParam(
     val carID: String? = null,
 //    val userID: String? = null,
     val carNo: String? = null,
     val eventType: Int? = null,
     val startTime: String? = null,
-    val endTime: String? = null
+    val endTime: String? = null,
+    val orgID: Int = orgId
 )
