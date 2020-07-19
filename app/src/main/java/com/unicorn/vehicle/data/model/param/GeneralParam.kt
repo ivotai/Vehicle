2750d0ea7c4ed6cb7902a3acde5583b2636fe825
ivotai <ivolianer2015@163.com>
2020-07-19 09:40:42 +0800

    添加 BasePoseInfo 的 create 方法。

--- app/src/main/java/com/unicorn/vehicle/data/model/param/GeneralParam.kt
+++ app/src/main/java/com/unicorn/vehicle/data/model/param/GeneralParam.kt
@@ -3,16 +3,21 @@ package com.unicorn.vehicle.data.model.param
 import com.unicorn.vehicle.app.di.Holder
 import com.unicorn.vehicle.app.helper.EncryptionHelper
 import com.unicorn.vehicle.app.loggedUser
+import com.unicorn.vehicle.data.model.BasePostInfo
 
 data class GeneralParam(
     val Param: String,
     val UserToken: String = loggedUser.userToken
-){
+) {
     companion object {
         fun create(a: Any): GeneralParam {
             val json = Holder.appComponent.gson().toJson(a)
             val param = EncryptionHelper.encrypt(json)
             return GeneralParam(Param = param)
+        }
+
+        fun create(): GeneralParam {
+            return create(a = BasePostInfo())
         }
     }
 }
