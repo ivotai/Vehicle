ee7392994b64f46cbb29a2abc8e3bdadcf11ba40
ivotai <ivolianer2015@163.com>
2020-07-19 09:52:44 +0800

    重构。

--- app/src/main/java/com/unicorn/vehicle/app/Globals.kt
+++ app/src/main/java/com/unicorn/vehicle/app/Globals.kt
@@ -4,8 +4,6 @@ import com.unicorn.vehicle.data.model.LoggedUser
 
 val uid: String get() = loggedUser.uid
 
-val orgId: Int get() = loggedUser.orgID
-
 lateinit var loggedUser: LoggedUser
 
 var isLogin = false
