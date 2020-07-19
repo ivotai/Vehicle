88e6442bd1c16e2ce1564e41d78e62217b096d15
ivotai <ivolianer2015@163.com>
2020-07-19 08:52:09 +0800

    不再使用 session

--- app/src/main/java/com/unicorn/vehicle/app/Globals.kt
+++ app/src/main/java/com/unicorn/vehicle/app/Globals.kt
@@ -2,8 +2,6 @@ package com.unicorn.vehicle.app
 
 import com.unicorn.vehicle.data.model.LoggedUser
 
-val sid: String get() = loggedUser.sid
-
 val uid: String get() = loggedUser.uid
 
 val orgId: Int get() = loggedUser.orgID
