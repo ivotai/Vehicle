afd95ed5bb312e0c8d0c4ff49cf467e92b69d57a
ivotai <ivolianer2015@163.com>
2020-07-19 08:56:06 +0800

    新 PageRequest.kt

--- app/src/main/java/com/unicorn/vehicle/data/model/base/PageRequest.kt
+++ app/src/main/java/com/unicorn/vehicle/data/model/base/PageRequest.kt
@@ -1,8 +1,10 @@
 package com.unicorn.vehicle.data.model.base
 
 import com.unicorn.vehicle.app.defaultPageSize
+import com.unicorn.vehicle.app.loggedUser
 
 data class PageRequest<T>(
+    val idCardNumber: String = loggedUser.uid,
     val pageNo: Int,        //页码，从1开始
     val pageSize: Int = defaultPageSize,
     val searchParam: T
